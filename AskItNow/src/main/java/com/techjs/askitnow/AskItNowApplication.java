902b13c364dfde679600667ad5bbb6f845438def
Sachin Singh <sachinsingh.sk13@gmail.com>
2020-05-22 20:38:40 +0530

    initial commit

---
+++ AskItNow/src/main/java/com/techjs/askitnow/AskItNowApplication.java
@@ -0,0 +1,13 @@
+package com.techjs.askitnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskItNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskItNowApplication.class, args);
+	}
+
+}
---
+++ AskItNow/src/test/java/com/techjs/askitnow/AskItNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techjs.askitnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskItNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
